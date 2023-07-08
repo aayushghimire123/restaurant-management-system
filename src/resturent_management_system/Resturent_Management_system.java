d35037269246abbd6c00a17497c487d27b3bd0f7
kaushal <kaushalranjit11@gmail.com>
2023-07-08 00:58:41 -0700

    layout

---
+++ src/resturent_management_system/Resturent_Management_system.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package resturent_management_system;
+
+/**
+ *
+ * @author HP
+ */
+public class Resturent_Management_system {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
