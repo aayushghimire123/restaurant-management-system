eb7e81b6e2e3fb10b1c8338b71a21469bac8e30c
kaushalRanjit <107354294+gigleson@users.noreply.github.com>
2023-08-18 12:40:45 +0545

    Add files via upload

---
+++ myConnection.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.*;
+
+
+/**
+ *
+ * @author asism
+ */
+public class myConnection {
+    
+    public static Connection myDatabase()
+    {
+        {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/RESTURENT","root","root");
+                    System.out.println("Database Connected!");
+                    return conn;
+        }
+        catch (Exception e)
+        
+        {
+            System.out.println(e);
+        return null;    
+        }
+        
+        }
+    }
+}
+    
+
